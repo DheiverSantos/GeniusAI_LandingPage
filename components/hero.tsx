562945eeb858610a19897c2ec3c2492659b473a1
Luiz Filipe de Lima Gomes Santos <luizfilipelgs@gmail.com>
2023-11-05 15:05:27 -0300

    fix: ajusta url video

--- components/hero.tsx
+++ components/hero.tsx
@@ -52,7 +52,7 @@ export default function Hero() {
             thumbWidth={1024}
             thumbHeight={576}
             thumbAlt="Modal video thumbnail"
-            video="https://seu-usuario.github.io/seu-repositorio/videos/video.mp4"
+            video="https://github.com/DheiverSantos/GeniusAI_LandingPage/tree/master/public/videos"
             videoWidth={1920}
             videoHeight={1080} 
           />
